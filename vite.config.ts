94217ed2fe326c12fd2a3f16cd47ef4d960e385b
fishros <sangxin2014@sina.com>
2022-01-29 04:56:55 +0000

    fix:修复url错误

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { env } from 'process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     AutoImport({
@@ -20,7 +22,7 @@ export default defineConfig({
   },
   define: {
     'process.env': {
-      'BASE_API': 'http://10.55.23.23:1234'
+      'BASE_API': '/calib'
     }
   }
 })
