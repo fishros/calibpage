3f05b1d9355d5bea6a051ae138dfda7088df9fdb
zebra <mzebra@foxmail.com>
2022-02-12 16:12:14 +0800

    fix: 时间格式化

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -39,7 +39,7 @@ export const formatGMT = (gmt: string) => {
     let date = new Date(gmt)
     let m = (date.getMonth() + 1), mm = '-' + (m < 10 ? '0' + m: m);
     let d = date.getDate(), dd = '-' + (d <  10 ? '0' + d : d)
-    let h = date.getHours(), hh = ' ' + (h < 10 ? '0' + d : d)
+    let h = date.getHours(), hh = ' ' + (h < 10 ? '0' + d : h)
     let i = date.getMinutes(), ii = ':' + (i < 10 ? '0' + i : i)
     let s = date.getSeconds(), ss = ':' + (s < 10 ? '0' + s : s)
     return date.getFullYear() + mm + dd + hh + ii + ss 
